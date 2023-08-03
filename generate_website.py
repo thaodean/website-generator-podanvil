ab8e05f9f532b2cf3d6e39a65786158c3a0932e1
Attila Toth <thaodean@gmail.com>
2023-08-03 08:48:57 +0200

    optimize

--- generate_website.py
+++ generate_website.py
@@ -1,29 +1,41 @@
-#!/usr/bin/env python3
-import os
-
-def generate_website(url):
-    page_content = {
-        'title': 'PodAnvil - Static Hosting Solution',
-        'header': 'Welcome to PodAnvil',
-        'main_content': 'We provide cheap, static, secure website hosting solutions in k8s pods.',
-        'premium_purchase_link': 'https://payment-gateway.com/podanvil',
-        'premium_purchase_text': """<div id="paypal-button-container-Premium"></div>""",
-        'standard_purchase_link': 'https://payment-gateway.com/podanvil',
-        'standard_purchase_text': """<div id="paypal-button-container-Standard"></div>""",
-    }
-
-    with open('index.html.template', 'r') as file:
-        template = file.read()
-
-    for placeholder, value in page_content.items():
-        template = template.replace(f'{{{{ {placeholder} }}}}', value)
-
-    new_dir_path = os.path.join(os.getcwd(), url)
-    os.makedirs(new_dir_path, exist_ok=True)
-
-    with open(os.path.join(new_dir_path, 'index.html'), 'w') as file:
-        file.write(template)
-
-    print("Website successfully generated!")
-
-generate_website('podanvil.com')
+page_content = {
+    'title': 'PodAnvil - Static Hosting Solution',
+    'header': 'Welcome to PodAnvil',
+    'main_content': 'We provide cheap, static, secure website hosting solutions in k8s pods.',
+    'standard_purchase_text': """
+        <div id="paypal-button-container-Standard"></div>
+        <script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
+        <script>
+            paypal.Buttons({
+                createOrder: function(data, actions) {
+                    // Set up the transaction
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: '30.00' // Update to match the price of your standard plan
+                            }
+                        }]
+                    });
+                }
+            }).render('#paypal-button-container-Standard');
+        </script>
+    """,
+    'premium_purchase_text': """
+        <div id="paypal-button-container-Premium"></div>
+        <script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
+        <script>
+            paypal.Buttons({
+                createOrder: function(data, actions) {
+                    // Set up the transaction
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: '70.00' // Update to match the price of your premium plan
+                            }
+                        }]
+                    });
+                }
+            }).render('#paypal-button-container-Premium');
+        </script>
+    """
+}
